622878466c9586a6fb059b6dce8a7165f61e1788
Gouled <77470644+gouledm@users.noreply.github.com>
2022-03-08 19:03:31 -0500

    Add files via upload
    First working version.

--- scraper.py
+++ scraper.py
@@ -1,13 +1,26 @@
 class scraper:
-    from urllib.request import urlretrieve
-    import requests
-    from bs4 import BeautifulSoup
-    url="https://eldenring.wiki.fextralife.com/Astrologer"
-    response = requests.get(url).text
+    def scrapeImage(self, link):
+        from urllib.request import urlretrieve
+        import requests
+        from bs4 import BeautifulSoup
+        url= link
+        response = requests.get(url).text
 
-    soup = BeautifulSoup(response, 'lxml')
+        soup = BeautifulSoup(response, 'lxml')
+        img = soup.find('td')
+        img = img.find('img')
+        image = img.attrs.get('src')
+        return image
 
-    div = soup.find('p').text
-    img = soup.find('td')
-    img = img.find('img')
-    image = img.attrs.get('src')
+    def scrapeP(self, link):
+        from urllib.request import urlretrieve
+        import requests
+        from bs4 import BeautifulSoup
+        url= link
+        response = requests.get(url).text
+
+        soup = BeautifulSoup(response, 'lxml')
+
+        p = soup.find('p').text
+        return p
+
