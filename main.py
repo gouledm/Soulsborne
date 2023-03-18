4a45d0bc2ef4a4623e81773a20c9c81507d143b0
gouledm <gouledm@my.yorku.ca>
2023-03-18 13:25:10 -0400

    Added slash command functionality

--- main.py
+++ main.py
@@ -4,22 +4,26 @@ from discord.ext import commands
 from autosearch import autosearch
 from scraper import scraper
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.members = True
+
+bot = discord.Bot(intents=intents)
 searcher = autosearch()
 scraper = scraper()
 
-@bot.command()
-async def find(ctx, *, arg):
+@bot.slash_command(name="find", description="Search for Elden Ring information")
+async def find(ctx, arg: str):
     link = autosearch.search(arg)
     image = scraper.scrapeImage(link)
     p = scraper.scrapeP(link)
     img = "https://eldenring.wiki.fextralife.com" + image
-    embed=discord.Embed(title="Elden Ring", description=p, color=0xBBA14F, footer = "Soulsborne")
+    embed=discord.Embed(title="Elden Ring", description=p, color=0xBBA14F)
     embed.set_image(url = img)
     await ctx.send(embed=embed)
 
 with open("token.txt", "r", encoding="utf-8") as f:
     token = f.read()
 
-bot.run(token)
 print("Running!")
+bot.run(token)
+
